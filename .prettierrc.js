be114827cc0a2fd214022fc25c70524e8c8aa6ae
Hossein Shakeri <hossein82shakeri@gmail.com>
2026-06-16 13:37:48 +0330

    Delete -> Additional files

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    semi: true,
-    trailingComma: 'es5', // Trailing commas only where valid in ES5 (objects, arrays, not function arguments).
-    singleQuote: true,
-    printWidth: 120,
-    tabWidth: 4,
-};
